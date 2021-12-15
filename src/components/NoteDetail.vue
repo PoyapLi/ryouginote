00290bb56bf48876f77748bbd07c4f299e874bbf
poyapli <963973854@qq.com>
2021-12-15 17:27:51 +0800

    添加笔记本详情页编辑区样式

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="note" class="detail">
-    <note-sidebar />
-    <div id="note-detail">
-      <h1>notebookId : {{ $route.query.notebookId }}</h1>
-      <h1>noteId : {{ $route.query.noteId }}</h1>
+    <note-sidebar></note-sidebar>
+    <div class="note-detail">
+      <div class="note-bar">
+        <span> 创建日期: 2天前 </span>
+        <span> 更新日期: 1分钟前 </span>
+        <span> 已保存 </span>
+        <span class="iconfont icon-delete" ></span>
+        <span class="iconfont icon-fullscreen" ></span>
+      </div>
+      <div class="note-title">
+        <input type="text" placeholder="输入标题">
+      </div>
+      <div class="editor">
+        <textarea v-show="true" placeholder="输入内容, 支持 markdown 语法"></textarea>
+        <div class="preview markdown-body" v-html="" v-show="false"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,7 +28,8 @@ import NoteSidebar from "@/components/NoteSidebar";
 export default {
   data () {
     return {
-      msg: '笔记详情页'
+      msg: '笔记详情页',
+      curNote:{}
     }
   },
   components:{
@@ -33,7 +46,9 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less">
+@import url(../assets/css/note-detail.less);
+
 #note {
   display: flex;
   align-items: stretch;
