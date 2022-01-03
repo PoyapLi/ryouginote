2f39f951721901e52f960a17c4f76c0e7e6c06fe
poyapli <963973854@qq.com>
2022-01-03 20:44:27 +0800

    解决 vue-router 新的报错

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
-// 解决控制台重复路由的报错
+// 解决控制台重复路由的报错,
 const originalReplace = Router.prototype.replace
 Router.prototype.replace = function replace(location) {
   return originalReplace.call(this, location).catch(err => err);
+}
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err);
 }
 
 export default new Router({
